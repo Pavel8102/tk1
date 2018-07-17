1f6c2bc684fc72c345336ab6c0f200c6fb78d8c9
Pavel8102 <41307445+Pavel8102@users.noreply.github.com>
2018-07-17 22:42:42 +0300

    Add files via upload

---
+++ turtle001.py
@@ -0,0 +1,34 @@
+import turtle
+import time
+import random
+print ("Risovalka")
+num_str = input("Enter the side number of the shape you want to draw: ")
+if num_str.isdigit():
+#if cifra not4 bukva
+    squares = int(num_str)
+angle = 180 - 180*(squares-2)/squares
+
+turtle.up
+
+x = 0
+y = 0
+turtle.setpos(x,y)
+
+numshapes = 8
+for x in range(numshapes):
+    turtle.color(random.random(),random.random(),random.random())
+    x += 5
+    y += 5
+    turtle.forward(x)
+    turtle.left(y)
+    for i in range(squares):
+        turtle.begin_fill()
+        turtle.down()
+        turtle.forward(140)
+        turtle.left(angle)
+        turtle.forward(140)
+        print (turtle.pos())
+        turtle.up()
+        turtle.end_fill()
+time.sleep(12)
+turtle.bye()
